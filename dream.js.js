8d708e8f6706065642cb59bab87d1a60c34fe955
decoezz <45102309+decoezz@users.noreply.github.com>
2018-11-19 09:22:42 +0200

    Add files via upload

---
+++ dream.js.js
@@ -0,0 +1,17 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`أبيع حالة ستريمينج ب10ألف`,"http://twitch.tv/Dream")
+client.user.setStatus("#IT WORKS!!")
+});
+
+
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
